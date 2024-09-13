7f1cf80daf5d1c85378ec83c7f580b9536221ee1
Tengis-vscode <tengis.b@orkhonschool.edu.mn>
2024-09-13 09:44:59 +0800

    first commit

---
+++ src/app.controller.ts
@@ -0,0 +1,22 @@
+import { Body, Controller, Get, Post, Req } from '@nestjs/common';
+import { AppService } from './app.service';
+import { AppValidator } from './app.validator';
+
+@Controller('number')
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get('getNumber')
+  getHello(): number {
+    return this.appService.getNumber();
+  }
+
+  @Post('setNumber')
+  setNumber(@Req() request: Request): any {
+    return this.appService.setNumber(request.body);
+  }
+  @Post('addNumber')
+  addNumber(@Body() body: AppValidator): number {
+    return this.appService.addNumber(body);
+  }
+}
